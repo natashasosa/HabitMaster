85f733b2470550a71784846e0142eadb5469278f
natashasosa <natasha.sosa@poatek.com>
2023-08-24 13:54:31 -0300

    First commit

---
+++ HabitMaster/HabitMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitMasterApp.swift
+//  HabitMaster
+//
+//  Created by Natasha Rebelo on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
